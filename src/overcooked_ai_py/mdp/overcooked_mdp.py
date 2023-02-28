ba2307d5472f77bb38e578520ee143f3a5fec5e0
Joana Raposo <55928045+JoaninhaRaposo@users.noreply.github.com>
2023-02-28 11:13:42 +0000

    Update overcooked_mdp.py

--- src/overcooked_ai_py/mdp/overcooked_mdp.py
+++ src/overcooked_ai_py/mdp/overcooked_mdp.py
@@ -741,10 +741,10 @@ class PlayerState(object):
     @property
     def pos_and_or(self):
         return (self.position, self.orientation)
-    @property
+    @property 
     def has_object(self):
         return self.held_object is not None
-
+    @property
     def get_object(self):
         assert self.has_object()
         return self.held_object
